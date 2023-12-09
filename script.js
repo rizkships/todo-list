6681010ca82b6ee6013aef619ed764cd1857c936
rizk <rizkdotai@gmail.com>
2023-12-09 13:46:29 +0200

    Create list object

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const listsContainer = document.querySelector('[data-lists')
 
 // create a variable to hold all our lists 
-let lists = ['name', 'todo']
+// we want an active-list class on whichever list is selected
+// we need to use id's and objects to do that
+let lists = [{
+   id: 1,
+   name: 'name'
+    }, {
+   id: 2,
+   name: 'todo'
+         }]
 
 // create a function that renders list 
 
@@ -9,8 +17,9 @@ function render() {
     clearElement(listsContainer)
     lists.forEach(list => {
         const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
         listElement.classList.add('list-name')
-        listElement.innerText = list
+        listElement.innerText = list.name
         listsContainer.appendChild(listElement)
     })
 }
